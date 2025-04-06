0c9259f1af96e8f961c41cdeff4bc567ed2d81a8
RafaelBonin00 <rafael.v.bonin@gmail.com>
2025-04-06 11:04:37 -0300

    commit json

--- script.js
+++ script.js
@@ -1,36 +1,6 @@
-const roupinhas = [
-    {
-      link_image: "/imagens/roupinhas/rosinha.jpeg",
-      Nome: "Vestidinho Doce Rosa",
-      Descricao: "Feito à mão com fio 100% algodão, ideal para os primeiros dias de vida. Tamanho RN."
-    },
-    {
-      link_image: ".//imagens/roupinhas/branca_2.jpeg",
-      "Nome": "Conjuntinho Brilho de Neve",
-      Descricao: "Delicado e suave como o primeiro abraço. Encomendas sob medida disponíveis."
-    },
-    {
-      link_image: "../imagens/roupinhas/branca_com_detalhe.jpeg",
-      "Nome": "Vestido Encanto de Pérola",
-      Descricao: "Detalhes em crochê que encantam. Perfeito para ocasiões especiais."
-    },
-    {
-      link_image: "./imagens/roupinhas/amarela.jpeg",
-      "Nome": "Conjuntinho Solzinho",
-      Descricao: "Amarelo alegre com toque de carinho. Acompanha toquinha combinando."
-    },
-    {
-      link_image: "./imagens/roupinhas/rosa_com_detalhe.jpeg",
-      "Nome": "Vestido Flor de Amor",
-      Descricao: "Rosa suave com detalhes encantadores. Um mimo cheio de ternura."
-    },
-    {
-      link_image: "./imagens/roupinhas/rosa.jpeg",
-      "Nome": "Conjuntinho Rosa Bebê",
-      Descricao: "Simples, delicado e cheio de amor. Crochê feito com fios de ternura."
-    }
-  ];
-
+fetch('roupinhas.json')
+.then(response => response.json())
+.then(roupinhas => {
   const galeria = document.getElementById("galeriaRoupinhas");
 
   roupinhas.forEach(peca => {
@@ -43,3 +13,7 @@ const roupinhas = [
     `;
     galeria.appendChild(div);
   });
+})
+.catch(error => {
+  console.error('Erro ao carregar o JSON:', error);
+});
